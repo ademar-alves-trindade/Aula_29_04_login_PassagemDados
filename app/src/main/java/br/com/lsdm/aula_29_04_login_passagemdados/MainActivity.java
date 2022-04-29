5f5effc5245f04e979975d4306ed6f7f392701c0
trindade <ademar.alves@unemat.br>
2022-04-29 04:12:58 -0300

    base_pronta

---
+++ app/src/main/java/br/com/lsdm/aula_29_04_login_passagemdados/MainActivity.java
@@ -0,0 +1,18 @@
+package br.com.lsdm.aula_29_04_login_passagemdados;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void entrar(View view) {
+    }
+}
---
+++ app/src/main/java/br/com/lsdm/aula_29_04_login_passagemdados/Tela2Activity.java
@@ -0,0 +1,14 @@
+package br.com.lsdm.aula_29_04_login_passagemdados;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Tela2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela2);
+    }
+}
